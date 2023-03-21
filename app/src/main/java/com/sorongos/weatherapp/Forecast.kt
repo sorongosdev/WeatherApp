5121ce6ee9fa1fcc0b7d0aeb149cf18414d4de49
Sorongos <dosora9@gmail.com>
2023-03-21 14:35:34 +0900

    서버에서 받아온 정보를 커스텀 형식을 통해 보기

---
+++ app/src/main/java/com/sorongos/weatherapp/Forecast.kt
@@ -0,0 +1,11 @@
+package com.sorongos.weatherapp
+
+data class Forecast(
+    val forecastDate : String,
+    val forecastTime : String,
+
+    var temperature: Double = 0.0,
+    var sky: String = "",
+    var precipitation: Int = 0,
+    var precipitationType: String = "",
+)
