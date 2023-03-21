efa0d852ba958482d39960dad391854727b455e8
Sorongos <dosora9@gmail.com>
2023-03-21 13:21:31 +0900

    add gradle: retrofit, gson converter

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WeatherApp
- 날씨를 알려주는 앱
+ 공공데이터포털의 날씨 api를 이용한 날씨를 알려주는 앱
+
+## Reference
+- ![img.png](img.png)
+
+## 키워드
+- retrofit & gson converter
+- 날씨 api
--- app/src/main/java/com/sorongos/weatherapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/weatherapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sorongos.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sorongos.weatherapp.databinding.ActivityMainBinding
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://apis.data.go.kr/")
     }
 }
