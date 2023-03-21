a1b6455841f1c187167fcdba3613bee58d0965dd
Sorongos <dosora9@gmail.com>
2023-03-21 14:38:10 +0900

    서버에서 정보들 받아오기
    *forecast.category는 Category?형
    -서버에서 받아오는 자료형: ~Entity
    -내가 보는 자료형: Forecast

--- app/src/main/java/com/sorongos/weatherapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/weatherapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.sorongos.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.sorongos.weatherapp.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -18,6 +22,66 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+        val service = retrofit.create(WeatherService::class.java)
 
+        service.getVillageForecast(
+            serviceKey = "DmBM2x0wpsJXPEO9ulpEtKw6+h8WozAD6uU7ngLMtXTiJiVIC6HxK80W4DU7/S+1mHutkaHsY1h4qjg72USKEQ==",
+            baseTime = "1100",
+            baseDate = "20230321",
+            nx = 55,
+            ny = 127
+        ).enqueue(object : Callback<WeatherEntity> {
+            override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
+                val forecastDateTimeMap = mutableMapOf<String, Forecast>()
+                val forecastList =
+                    response.body()?.response?.body?.items?.forecastEntities.orEmpty()
+
+                for (forecast in forecastList) {
+                    Log.e("Forecast", forecast.toString())
+
+                    if (forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"] == null) {
+                        forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"] =
+                            Forecast(
+                                forecastDate = forecast.forecastDate,
+                                forecastTime = forecast.forecastTime
+                            )
+                    }
+                    forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"]?.apply {
+                        when (forecast.category) {
+                            Category.POP -> precipitation = forecast.forecastValue.toInt()
+                            Category.PTY -> precipitationType = transformRainType(forecast)
+                            Category.SKY -> transformSky(forecast)
+                            Category.TMP -> temperature = forecast.forecastValue.toDouble()
+                            else -> {}
+                        }
+                    }
+                }
+                Log.e("Forecast", forecastDateTimeMap.toString())
+            }
+
+            override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+    }
+
+    private fun transformRainType(forecast: ForecastEntity): String {
+        return when (forecast.forecastValue.toInt()) {
+            0 -> "없음"
+            1 -> "비"
+            2 -> "비/눈"
+            3 -> "눈"
+            4 -> "소나기"
+            else -> ""
+        }
+    }
+    private fun transformSky(forecast: ForecastEntity): String {
+        return when (forecast.forecastValue.toInt()) {
+            1 -> "맑음"
+            3 -> "구름많음"
+            4 -> "흐림"
+            else -> ""
+        }
     }
 }
